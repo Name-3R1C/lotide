c9fe979782d277815a6444e6c7116f9197d67aea
Name-3R1C <ericfeng94@hotmail.com>
2023-03-26 01:54:20 -0700

    implemented tails function

---
+++ tail.js
@@ -0,0 +1,39 @@
+const assertEqual = function(actual, expected) {
+  // Emoji
+  let correct = String.fromCodePoint(0x2705) + String.fromCodePoint(0x2705) + String.fromCodePoint(0x2705);
+  let wrong = String.fromCodePoint(0x274C) + String.fromCodePoint(0x274C) + String.fromCodePoint(0x274C);
+  let ans = (actual === expected);
+  if (ans) {
+    console.log(`${correct} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${wrong} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(arr) {
+  if (arr.length <= 1) {
+    return [];
+  }
+  let ans = arr.slice(1);
+  return ans;
+};
+
+// Test Case 1: Check the returned array elements
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
+// Test Case: Check for array size of 1 and empty array
+const oneElm = tail([1]);
+assertEqual(oneElm.length, 0);
+assertEqual(oneElm[0], undefined);
+
+const empty = tail([]);
+assertEqual(empty.length, 0);
+assertEqual(empty[0], undefined);
