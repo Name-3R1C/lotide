2cbc2c5b14e9d6a51477ef8a54314f31ba5042d8
Name-3R1C <ericfeng94@hotmail.com>
2023-03-26 21:06:09 -0700

    implemented function to compare two arrays

---
+++ eqArrays.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  // Emoji
+  let correct = String.fromCodePoint(0x2705) + String.fromCodePoint(0x2705) + String.fromCodePoint(0x2705);
+  let wrong = String.fromCodePoint(0x274C) + String.fromCodePoint(0x274C) + String.fromCodePoint(0x274C);
+  let ans = (actual === expected);
+  if (ans) {
+    console.log(`${correct} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${wrong} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  for(let i = 0; i < arr1.length; i++) {
+    if(arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
