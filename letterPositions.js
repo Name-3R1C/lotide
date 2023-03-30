585ed28925a0d91369fb421b2fc5c7d82d4efe29
Name-3R1C <ericfeng94@hotmail.com>
2023-03-29 23:38:32 -0700

    Updated for loop that iterate object

--- letterPositions.js
+++ letterPositions.js
@@ -40,7 +40,7 @@ const letterPositions = function(sentence) {
 let res = letterPositions("hello");
 let i = 0;
 let expectedRes = [[0],[1],[2,3],[4]];
-for (const item of Object.keys(res)) {
+for (const item in res) {
   assertArraysEqual(res[item],expectedRes[i]);
   i++;
 }
@@ -49,7 +49,7 @@ console.log("\n ");
 res = letterPositions("lighthouse in the house");
 i = 0;
 expectedRes = [[0],[1, 11],[2],[3, 5, 15, 18],[4, 14],[6, 19],[7, 20],[8, 21],[9, 16, 22],[12]];
-for (const item of Object.keys(res)) {
+for (const item in res) {
   assertArraysEqual(res[item],expectedRes[i]);
   i++;
 }
