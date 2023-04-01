7af957d4204c1bdc711b50b9371b20ccf8c82e85
Name-3R1C <ericfeng94@hotmail.com>
2023-04-01 15:19:01 -0700

    Implemented findKey function that given an object, find the correct key vale if found else return undefined

---
+++ findKey.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  let ans = (actual === expected);
+  if (ans) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  for (const item in object) {
+    if (callback(object[item])) {
+      return item;
+    }
+  }
+  return undefined;
+};
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma"); // => "noma"
