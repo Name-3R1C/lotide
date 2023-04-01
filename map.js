b3332d070fea2159bb5acd4f34748f33deaf114c
Name-3R1C <ericfeng94@hotmail.com>
2023-04-01 14:15:40 -0700

    implemented map function

---
+++ map.js
@@ -0,0 +1,37 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(firstArr, secArr) {
+  const ans = eqArrays(firstArr, secArr);
+  if (ans) {
+    console.log(`✅✅✅ Assertion Passed: ${firstArr} === ${secArr}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${firstArr} !== ${secArr}`);
+  }
+};
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+const results1 = map(words, word => word[0]);
+assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']);
+
+const nums = [1, 2, 3, 4, 5];
+const results2 = map(nums, num => num > 3);
+assertArraysEqual(results2, [false, false, false, true, true]);
