616cfd16da62a12b7417957f910d6b6c65f19228
Name-3R1C <ericfeng94@hotmail.com>
2023-04-18 23:10:23 -0700

    refactored assertArrayEqual, seperated function and test case'

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(firstArr, secArr) {
   const ans = eqArrays(firstArr, secArr);
@@ -20,8 +9,4 @@ const assertArraysEqual = function(firstArr, secArr) {
   }
 };
 
-assertArraysEqual([1,2,3],[1,2,3]); // true
-assertArraysEqual([],[]); // true
-assertArraysEqual([1,2],[1,2,3]); // false
-assertArraysEqual([],[1,2,3]); // false
-assertArraysEqual(["1",2,3,4],[1,2,3,4]); // false
+module.exports = assertArraysEqual;
