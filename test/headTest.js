65d4e1d4fdb2b06f5fc7f636347e848831426efa
Name-3R1C <ericfeng94@hotmail.com>
2023-04-18 23:34:27 -0700

    converted headTest.js to use Mocha and Chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,27 @@
+const assert = require('chai').assert;
 const head = require('../head');
-const assertEqual = require('../assertEqual');
 
-assertEqual(head([5,6,7]), 5); // pass
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // pass
-assertEqual(head([1]), 1); // pass
-assertEqual(head([]), 5); // fail
+describe("#head", () => {
+  it("return 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("return '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("return 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5,6,7]), 5);
+  });
+  it('return "Hello" for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+  it("return 1 for [1]", () => {
+    assert.strictEqual(head([1]), 1);
+  });
+  
+  it("return undefined", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+});
