c882b9162aed8d0f6dfd182c223bd75e37136660
Name-3R1C <ericfeng94@hotmail.com>
2023-04-19 00:10:04 -0700

    refactored tailTest and middleTest to use Mocha & Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,22 +1,16 @@
+const assert = require('chai').assert;
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+describe("#tail", () => {
+  it('return ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+  it('return undefined for [1]', () => {
+    assert.deepEqual(tail([1]), []);
+  });
 
-// Test Case: Check for array size of 1 and empty array
-const oneElm = tail([1]);
-assertEqual(oneElm.length, 0);
-assertEqual(oneElm[0], undefined);
-
-const empty = tail([]);
-assertEqual(empty.length, 0);
-assertEqual(empty[0], undefined);
+  it('return [] for []', () => {
+    assert.deepEqual(tail([]), []);
+  });
+});
